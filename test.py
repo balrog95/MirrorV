aaf5385961eecbab9034b970d64b7cb132a3ca9f
dylan.furlong <dylan.furlong@frogslayer.com>
2017-01-19 22:05:40 -0600

    some minor experimenting, got a clock to display crudely

--- test.py
+++ test.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python
 
-from tkinter import Tk, Frame, BOTH
+from tkinter import Tk, Frame, Label, BOTH
+import time
 
-class Example(Frame):
+class Display(Frame):
+
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")
         self.parent = parent
+        self.time1 = ''
         self.initUI()
 
     def initUI(self):
-        self.parent.title("Simple")
+        self.parent.title("MirrorV")
         self.pack(fill=BOTH, expand=1)
         self.makeFullscreen()
+        self.displayClock()
     
-    def makeFullscreen(self, event = None):
+    def makeFullscreen(self):
         self.parent.attributes("-fullscreen", True);
+
+    def displayClock(self):
+        self.clock = Label(self.parent, font=('times', 20, 'bold'), bg='green')
+        self.clock.pack(fill=BOTH, expand=1)
+        self.tick()
+
+    def tick(self):
+        time2 = time.strftime('%H:%M:%S')
+        if time2 != self.time1:
+            self.time1 = time2
+            self.clock.config(text=time2)
+        self.clock.after(200, self.tick)
 
 def main():
     root = Tk()
     root.geometry("250x150+300+300")
-    app = Example(root)
+    app = Display(root)
     root.mainloop()
 
 if __name__ == '__main__':
